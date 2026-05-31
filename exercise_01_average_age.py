df4b0c5f44c9578f72e1f843c0d44bc1a8b210b9
aviadsi100 <aviadsi100@gmail.com>
2026-05-31 23:54:11 +0300

    Add files via upload

---
+++ exercise_01_average_age.py
@@ -0,0 +1,11 @@
+# Exercise 01 - Average age of 3 people
+# Create 3 variables according to 3 people.
+# The variable name is the first name and the value is the age.
+# Save the average age in a new variable and print it.
+
+avi = 22
+noa = 19
+dana = 25
+
+average_age = (avi + noa + dana) / 3
+print(average_age)
